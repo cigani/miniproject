80ff31671f0e05b0a7d04628bfca43aeb114162f
Michael Jaquier <michaeljaquier@michaels-macbook-pro.home>
2016-04-19 18:56:44 +0200

    Add step current TODO: Plotting

--- src/hhBase.py
+++ src/hhBase.py
@@ -1,3 +1,10 @@
+"""
+Base HH model for simulations. Import it this module for all other project
+parts.
+
+"""
+
+
 import brian2 as b2
 import sys
 import matplotlib.pyplot as plt
@@ -40,3 +47,48 @@ def hhNeuron(current, simtime):
      dn/dt = alphan*(1-n)-betan*n : 1
      dvm/dt = membrane_Im/C : volt
      '''
+
+    neuron = b2.NeuronGroup(1,eqs,method='exponential_euler')
+
+    # parameter initial
+    # TODO: find correct paramaters for m,h,n
+    neuron.vm = 0
+    neuron.m = 0
+    neuron.h = 0
+    neuron.n = 0
+
+    # track values
+    stateVals = (b2.StateMonitor(neuron, ['vm', 'i_e', 'm', 'n', 'h'],
+    record = True))
+
+    # running simulatin
+    b2.run(simtime)
+
+    return stateVals
+
+def hhStep(its=0,ite=150,iamp=0,te=200,sr=1, doPlot=False):
+
+    """ Step current for hh base model.
+
+    Args:
+        its (float): start of step [ms]
+        ite (float): end of step [ms]
+        iamp (float): amplitude of step [uA]
+        te (float): end of sim [ms]
+        sr (float): sampling rate [ms]
+        doPlot (binary): do plot [T/F]
+
+    Return:
+        StateMonitor: Brian 2 fields:
+        ['vm', 'i_e', 'm', 'n', 'h']
+    """
+
+    # step sample rate
+    tmp = np.zeros(te)*b2.uamp
+    tmp[int(its):int(ite)] = iamp*b2.uamp
+    current = b2.TimedArray(tmp,dt=sr*b2.ms)
+
+    stateVals = hhNeuron(current,te*b2.ms)
+
+    # TODO: Add plotting stuff
+
