0a6d409fa224beb78344d9b0825bdee7b4f37c3c
Michael Jaquier <michaeljaquier@tsf-460-wpa-3-236.epfl.ch>
2016-04-21 11:56:10 +0200

    taus vs volt. TODO: what are the units of tau 1/s?

--- src/tau_plot.py
+++ src/tau_plot.py
@@ -11,6 +11,14 @@ print hex.tm[0]
 traceall = np.append(hex.minf[0], [hex.ninf[0], hex.hinf[0]])
 nrmfactor = np.max(traceall)/b2.mV
 
-
-plt.plot(hex.vm[0]/b2.mV, hex.tm[0]/b2.ms, 'blue', lw=2)
+plt.subplot(311)
+plt.plot(hex.vm[0]/b2.mV, hex.tn[0]/b2.ms, 'blue', lw=2)
+plt.ylabel('n tau')
+plt.subplot(312)
+plt.plot(hex.vm[0]/b2.mV, hex.tm[0]/b2.ms, 'black', lw=2)
+plt.ylabel('m tau')
+plt.subplot(313)
+plt.plot(hex.vm[0]/b2.mV, hex.th[0]/b2.ms, 'red', lw=2)
+plt.ylabel('h tau')
+plt.xlabel('v [mV]')
 plt.show()
